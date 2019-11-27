deff3104cf782ee3610937960266f4e32dd595f0
Batuhan Çağlayan <batuhancaglayan1@gmail.com>
2019-11-27 16:19:53 +0300

    change on hotswap
    change on hotswap

--- socket-io/src/index.js
+++ socket-io/src/index.js
@@ -2,17 +2,15 @@ import http from 'http';
 import express from 'express';
 import io from 'socket.io';
 import path from 'path';
-import cors from 'cors';
 
-//const allowedOrigins = "http://localhost:* http://127.0.0.1:*";
-const socketPath ='/socket';
 const port = 9090;
-const app = express(cors({ origin: '*' , credentials :  false}));
+const app = express();
 
 const server = http.createServer(app);
 const socket = io(server);
   
   app.get('/', function(req, res) {
+    console.log("aq");
     res.header('Access-Control-Allow-Origin', '*');
     res.sendFile(path.resolve('./src/view/index.html'));
   });
@@ -22,17 +20,21 @@ const socket = io(server);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-  
-  socket.on('connect', function(client) {
-    console.log('A user connected');
-    
-    client.on('disconnect', function () {
-      console.log('A user disconnected');
-    });
-  });
-  
+
   server.listen(port, () => {
     console.log('Server running at http://127.0.0.1:' + port);
+  });
+  
+  socket.on('connect', function(client){
+    console.log('connection');
+    // Success!  Now listen to messages to be received
+    client.on('message',function(event){
+      console.log('Received message from clientss!', event);
+    });
+    client.on('disconnect',function(){
+      //clearInterval(interval);
+      console.log('Server has disconnected');
+    });
   });
   
   export default server;
